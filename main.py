1e66e31ccc4c8ed9281972e21ec1891ae21823af
Pedro Garcia <pedrognunes139@gmail.com>
2022-07-08 20:57:48 -0300

    decode function

---
+++ main.py
@@ -0,0 +1,27 @@
+import re
+
+
+
+instList = open("instrucoes.txt","r")
+instList = instList.readlines()
+
+
+
+"""
+Recebe uma instrucao e retorna uma tupla contendo a oprecao e
+os registradores.O segundo item da tupla sempre sera o registrador de destino (rd).
+"""
+def inst_decoder(inst) :
+    op = inst[0:3].strip()
+    regPattern = "R\d+"
+    regList = re.findall(regPattern, inst)
+    return (op,*regList)
+     
+
+
+
+
+
+
+
+
---
+++ reservationStation.py
@@ -0,0 +1,11 @@
+class ReservationStation:
+    def __init__(self,name):
+        self.name = name
+        self.busy = 0
+        self.op = None
+        self.vj = None
+        self.vk = None
+        self.qj = None
+        self.qk = None
+
+    
