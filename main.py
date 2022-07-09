ccd3c77bcd66d807d0a6d38505454e7ae83b69fa
Pedro Garcia <pedrognunes139@gmail.com>
2022-07-08 21:01:56 -0300

    mod

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ instList = instList.readlines()
 
 """
 Recebe uma instrucao e retorna uma tupla contendo a oprecao e
-os registradores.O segundo item da tupla sempre sera o registrador de destino (rd).
+os registradores.O segundo item da tupla sempre sera o registrador de destino (rd)
 """
 def inst_decoder(inst) :
     op = inst[0:3].strip()
